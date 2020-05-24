1f20c4ab93f7594a3fc21869395e3bf0e3d60720
FelipeQueFez <felipequefez@gmail.com>
2020-05-24 01:55:10 -0300

    unsed

--- lib/views/shared/spacer_box.dart
+++ lib/views/shared/spacer_box.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 
 class SpacerBox {
   //  Vertical Spaces
-  static const v4 = SizedBox(
-    height: 4,
-  );
-  static const v8 = SizedBox(
-    height: 8,
-  );
   static const v10 = SizedBox(
     height: 10,
   );
@@ -22,17 +16,6 @@ class SpacerBox {
   );
 
   // Horizontal Spaces
-  static const h5 = SizedBox(
-    width: 5,
-  );
-  static const h8 = SizedBox(
-    width: 8,
-  );
-
-  static const h16 = SizedBox(
-    width: 15,
-  );
-
   static const h10 = SizedBox(
     width: 10,
   );
